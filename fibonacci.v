889fa23290c1f047e69f1a902496a35580f707f2
kibarra89 <57735606+kibarra89@users.noreply.github.com>
2019-12-02 18:16:48 -0800

    updated from 8bitworkshop.com

--- fibonacci.v
+++ fibonacci.v
@@ -1,18 +1,8 @@
-module fibonocci( clk, y, reg [3:0]);
-  input clk,;
-    if[(n == 0) || (n ==1 )] begin
-      fib = n;
-    end
-    else begin
-      fib = fib(n - 1)+fib(n + 2) ;
-    end
-  endtask
-  initial begin
-    f0i (int i = 0; i <= 100; i++) begin
-      $display("Fibnocci = %d", fib(i);
-               end
-               #10;
-               $finish
-               end
-               endmodule
+module fibonocci ( clk, y, reg [3:0] );
+  input clk;
+  output reg [3:0] y;
+               
+
+
+endmodule
                
